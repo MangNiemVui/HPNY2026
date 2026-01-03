a240e875cb3d85e91605cce2a3244d024f3b20f1
Bé Thư cute <117712658+MangNiemVui@users.noreply.github.com>
2026-01-03 19:46:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-// ===== Firebase config =====
+// config.js
+// Firebase
 window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyAiC7qpEUHWew2BCdW0_Vn1ryOeXv8OZYU",
   authDomain: "hpny-1cc4b.firebaseapp.com",
@@ -9,17 +10,15 @@ window.FIREBASE_CONFIG = {
   measurementId: "G-H345DYP77D"
 };
 
-// ===== Owner settings =====
-// Owner key để filter dữ liệu theo "chủ sở hữu" (bạn để key owner của bạn)
+// Owner settings
 window.OWNER_KEY = "bethucute";
+// UID bạn chụp hình: pZkAh7riPDVnHTTwWV4V77tTnBk2
+window.OWNER_UID = "pZkAh7riPDVnHTTwWV4V77tTnBk2";
 
-// UID của tài khoản Google Owner (sau khi login sẽ hiện UID trong alert)
-window.OWNER_UID = "PASTE_OWNER_UID_HERE";
-
-// ===== EmailJS config =====
+// EmailJS (từ ảnh bạn gửi)
 window.EMAILJS_PUBLIC_KEY = "BkywBTvqF7dlOrqnh";
 window.EMAILJS_SERVICE_ID = "service_s5ecpfq";
 window.EMAILJS_TEMPLATE_ID = "template_zpr88bw";
 
-// email nhận (chủ sở hữu)
-window.OWNER_EMAIL_TO = "phanthu27112002@gmail.com";
+// Email nhận (Gmail chủ sở hữu)
+window.OWNER_EMAIL = "phanthu27112002@gmail.com";
